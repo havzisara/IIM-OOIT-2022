0b4c373415a0ca1a639b010cea077e260e3eb2d5
Sara <Sara@DESKTOP-1CII6GH>
2022-11-29 16:12:14 +0100

    First Commit

---
+++ OOITiim/src/geometry/Point.java
@@ -0,0 +1,53 @@
+package geometry;
+
+public class Point {
+	private int x;
+	private int y;
+	private boolean selected;
+
+	public Point() {
+		
+	}
+	
+	public Point(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public Point(int x, int y, boolean selected) {
+		this.x=x;
+		this.y=y;
+		this.selected=selected;
+	}
+	
+	public double distance(int x, int y) {
+		int dX = this.x - x;
+		int dY = this.y - y;
+		double d = Math.sqrt(dX*dX + dY*dY);
+		return d;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int newY) {
+		y = newY;
+	}
+
+	public boolean getSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+}
